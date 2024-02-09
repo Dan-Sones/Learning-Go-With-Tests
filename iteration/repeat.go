f2cd914f89fde1639cf46d05f8d54278cd946269
Dan-Sones <daniel.sones123@gmail.com>
2024-02-09 11:46:06 +0000

    Refactor tests and try with inbuilt strings method

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,9 +1,8 @@
 package iteration
 
+import "strings"
+
 func Repeat(character string, charCount int) string {
-	var repeated string
-	for i := 0; i < charCount; i++ {
-		repeated += character
-	}
-	return repeated
+
+	return strings.Repeat(character, charCount)
 }
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -2,16 +2,33 @@ package iteration
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 )
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a", 6)
+
 	expected := "aaaaaa"
 
+	t.Run("pass using my solution", func(t *testing.T) {
+		repeated := Repeat("a", 6)
+
+		assertOutputCorrect(t, repeated, expected)
+
+	})
+
+	t.Run("pass using inbuilt string method", func(t *testing.T) {
+		repeated := strings.Repeat("a", 6)
+		assertOutputCorrect(t, repeated, expected)
+	})
+}
+
+func assertOutputCorrect(t testing.TB, repeated, expected string) {
+	t.Helper()
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
 	}
+
 }
 
 func BenchmarkRepeat(b *testing.B) {
