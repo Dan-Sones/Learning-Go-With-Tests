d0e8fe2ca4e91e356678ca94b3370524a1b2efc8
Dan-Sones <daniel.sones123@gmail.com>
2024-02-09 11:04:09 +0000

    new test cases for no string supplied

--- hello_test.go
+++ hello_test.go
@@ -2,11 +2,32 @@ package main
 
 import "testing"
 
-func TestHello(t *testing.T) {
-	got := Hello("Daniel")
-	want := "Hello, Daniel"
+//Write a test
+//Make the compiler pass
+//Run the test, see that it fails and check the error message is meaningful
+//Write enough code to make the test pass
+//Refactor
 
+func TestHello(t *testing.T) {
+	t.Run("Saying Hello to people", func(t *testing.T) {
+		got := Hello("Daniel")
+		want := "Hello, Daniel"
+
+		asserCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		asserCorrectMessage(t, got, want)
+	})
+}
+
+func asserCorrectMessage(t testing.TB, got, want string) {
+	// Lets the test suite know this is a helper method,
+	// If the test fails, the pointer won't be here, it will be in the parent function
+	t.Helper()
 	if got != want {
-		t.Errorf("got %q want %q", got, want)
+		t.Errorf("got %q want %q ", got, want)
 	}
 }
