d58d922cd447b66d9d0a7b997961aadf0599a717
Dan-Sones <daniel.sones123@gmail.com>
2024-02-09 14:53:19 +0000

    Add triangle

--- structs-methods-interfaces/shapes_test.go
+++ structs-methods-interfaces/shapes_test.go
@@ -12,47 +12,25 @@ func TestPerimeter(t *testing.T) {
 	}
 }
 
-//func TestArea(t *testing.T) {
-//
-//	checkArea := func(t testing.TB, shape Shape, want float64) {
-//		t.Helper()
-//		got := shape.Area()
-//		if got != want {
-//			t.Errorf("got %g want %g", got, want)
-//		}
-//	}
-//
-//	t.Run("rectangles", func(t *testing.T) {
-//		rectangle := Rectangle{12, 6}
-//		checkArea(t, rectangle, 72.0)
-//	})
-//
-//	t.Run("circles", func(t *testing.T) {
-//		circle := Circle{10}
-//		checkArea(t, circle, 314.1592653589793)
-//	})
-//
-//}
-
 func TestArea(t *testing.T) {
 	// Table-Driven Tests Approach
 
-	// Create a slice of structs, where each element of the slice must:
-	// Have a shape property
-	// Have a want property
+	// Create a slice of structs
 	areaTests := []struct {
-		shape Shape
-		want  float64
+		shape   Shape
+		hasArea float64
 	}{
 		// Add all the shapes here
-		{Rectangle{12, 6}, 72.0},
-		{Circle{10}, 314.1592653589793},
+		{Rectangle{Width: 12, Height: 6}, 72.0},
+		{Circle{Radius: 10}, 314.1592653589793},
+		{Triangle{Base: 12, Height: 6}, 36.0},
 	}
 
+	// Iterate through each Shape and check to see if area is correct
 	for _, tt := range areaTests {
 		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("got %g want %g", got, tt.want)
+		if got != tt.hasArea {
+			t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
 		}
 	}
 
