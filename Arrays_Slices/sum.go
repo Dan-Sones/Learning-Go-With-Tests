5e8832974ce79730311b53cffd7f2d50b12fe5eb
Dan-Sones <daniel.sones123@gmail.com>
2024-02-09 13:57:37 +0000

    slices

--- Arrays_Slices/sum.go
+++ Arrays_Slices/sum.go
@@ -1,10 +1,38 @@
 package Arrays_Slices
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
-
 	for _, number := range numbers {
 		sum += number
 	}
 	return sum
+}
+
+//func SumAll(numbersToSum ...[]int) []int {
+//	lengthOfArray := len(numbersToSum)
+//
+//	// make allows us to create a new slice
+//	// Where the second arguement is the initial length
+//	sums := make([]int, lengthOfArray)
+//
+//	for i, numbers := range numbersToSum {
+//		sums[i] = Sum(numbers)
+//	}
+//
+//	return sums
+//
+//}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int                         // create an empty slice
+	for _, numbers := range numbersToSum { // For each array passed in
+
+		if len(numbers) == 0 { // if it is empty, then just append 0, there is no point doing a calc
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail)) // append the result of Sum() to the sums array
+		}
+	}
+	return sums
 }
