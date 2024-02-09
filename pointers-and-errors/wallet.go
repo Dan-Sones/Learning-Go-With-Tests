9e4438c6a9b41ab22a3203b70342a56d6b9a6b59
Dan-Sones <daniel.sones123@gmail.com>
2024-02-09 15:13:16 +0000

    withdraw functionality

--- pointers-and-errors/wallet.go
+++ pointers-and-errors/wallet.go
@@ -1,15 +1,31 @@
 package pointers_and_error
 
+import "fmt"
+
+type Bitcoin int
+
 type Wallet struct {
 	// a lowercase 'b' indicates it is private
-	balance int
+	balance Bitcoin
 }
 
-func (w *Wallet) Deposit(amount int) {
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return (*w).balance
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
 }
--- pointers-and-errors/wallet_test.go
+++ pointers-and-errors/wallet_test.go
@@ -3,14 +3,31 @@ package pointers_and_error
 import "testing"
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
 
-	wallet.Deposit(10)
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
 
-	got := wallet.Balance()
-	want := 10
+		wallet.Deposit(Bitcoin(10))
 
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
-	}
+		got := wallet.Balance()
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+
+		wallet.Withdraw(Bitcoin(10))
+
+		got := wallet.Balance()
+
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
 }
