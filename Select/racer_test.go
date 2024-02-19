10b1be511d8adf43cd1db793614c7424ada59abf
Dan-Sones <daniel.sones123@gmail.com>
2024-02-19 14:41:11 +0000

    racer without selects

---
+++ Select/racer_test.go
@@ -0,0 +1,36 @@
+package Select
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+
+	slowServer := makeDelayedServer(20 * time.Millisecond)
+	fastServer := makeDelayedServer(0 * time.Millisecond)
+
+	// defer runs at the end of the functions execution
+	defer slowServer.Close()
+	defer fastServer.Close()
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
+
+	want := fastURL
+	got := Racer(slowURL, fastURL)
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		time.Sleep(delay)
+		writer.WriteHeader(http.StatusOK)
+	}))
+}
