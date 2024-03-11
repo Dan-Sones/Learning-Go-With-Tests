1b9928d5f3f360ce2cc1b7d071e1f962d0c57ca1
Dan-Sones <daniel.sones123@gmail.com>
2024-03-11 15:40:25 +0000

    Solution up to III

---
+++ romanNumeralKata/numeral.go
@@ -0,0 +1,14 @@
+package romanNumeralKata
+
+import "strings"
+
+func ConvertToRoman(arabic int) string {
+
+	var result strings.Builder
+
+	for i := 0; i < arabic; i++ {
+		result.WriteString("I")
+	}
+
+	return result.String()
+}
